1254a5d387c2716ef84e6df9645d39da27bd7a1c
SoRoot <lukas.ungerland@gmail.com>
2016-06-27 11:03:50 +0200

    signup error msg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
 	def create
 		@user = User.new(user_params)
-		@user.valid?
 		if @user.save
 			session[:user_id] = @user.id
 			redirect_to '/'
@@ -15,7 +14,6 @@ class UsersController < ApplicationController
 	end
 
 	private
-
 	def user_params
 		params.require(:user).permit(:username, :password, :password_confirmation)
 	end
