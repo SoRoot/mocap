afe44c981550357da903b232b7c1d12426b7c031
Marie-Luise Lux <Marie-Luise.Lux@web.de>
2016-08-26 22:40:34 +1000

    nothing accomplished

--- app/models/motion.rb
+++ app/models/motion.rb
@@ -16,7 +16,6 @@ class Motion
 
   def self.search(actors, search_actor, search_gender, search_role, search_mood, search_description)    #search the database for the corresponding motions
     found = Motion.all
-    t = found
     unless search_gender.blank?
       unless found.blank?
         found.each do |x|
@@ -24,12 +23,11 @@ class Motion
             a = actors.where(:_id => rec.actor.to_s).first                    #from the MotionRecord you are able to get the actor
             
             unless (a.gender == search_gender)                                  #if the gender of the actor is not the gender searched for
-              t = t.not_in(:motion_record.to_s => rec._id)              #take out all motions that belong to that MotionRecord
+              found = found.not_in(:motion_record.to_s => rec._id)                      #take out all motions that belong to that MotionRecord
             end
         end
       end
     end
-    found = t
 
     unless search_actor.blank?
       found = found.where(:actor => search_actor)
